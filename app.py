2872f98f9ddc03b740b218d50cf7e929f86b5ac0
jmadde882 <jack_madden@comcast.com>
2023-11-29 15:53:54 -0500

    Add home page, create post, contact, and advertisements page.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -10,6 +10,30 @@ def home():
         {"title": "Second Post", "content": "This is the content of my second post"}
     ]
     return render_template('home.html', posts=posts)
+
+
+@app.route('/create_post', methods=['GET', 'POST'])
+def create_post():
+    if request.method == 'POST':
+        # Handle the form submission
+        title = request.form['title']
+        content = request.form['content']
+        print("New Post Created:", title, content)
+        # Later, you'll save this data to a database
+
+        return redirect(url_for('home'))  # Redirect to the home page after submission
+
+    return render_template('create_post.html')  # Render the form page for GET requests
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', title='Contact Us')
+
+
+@app.route('/advertise')
+def advertise():
+    return render_template('advertise.html', title='Advertise with Us')
 
 
 if __name__ == '__main__':
