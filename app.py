91d43d97790bdb5dd5b13684d1c77cb842e830ab
jmadde882 <jack_madden@comcast.com>
2023-12-01 18:00:58 -0500

    Add ability to preview post, edit post, and delete post.
    Add ability for user to see post.

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import os
 from datetime import datetime
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SECRET_KEY'] = os.urandom(24)  # Generate a random secret key
 db = SQLAlchemy(app)
 
 
@@ -27,13 +29,37 @@ def home():
 @app.route('/create_post', methods=['GET', 'POST'])
 def create_post():
     if request.method == 'POST':
-        post_title = request.form['title']
-        post_content = request.form['content']
-        new_post = Post(title=post_title, content=post_content)
-        db.session.add(new_post)
-        db.session.commit()
-        return redirect(url_for('home'))
-    return render_template('create_post.html')
+        session['temp_post'] = {
+            'title': request.form['title'],
+            'content': request.form['content']
+        }
+        return redirect(url_for('preview_post'))
+
+    posts = Post.query.all()  # Fetch all posts from the database
+    return render_template('create_post.html', posts=posts)
+
+
+@app.route('/preview_post', methods=['GET', 'POST'])
+def preview_post():
+    if request.method == 'POST':
+        # Save the post from the session to the database
+        new_post_data = session.get('temp_post')
+        if new_post_data:
+            new_post = Post(title=new_post_data['title'], content=new_post_data['content'])
+            db.session.add(new_post)
+            db.session.commit()
+            session.pop('temp_post', None)  # Clear the temporary post from the session
+            return redirect(url_for('create_post'))
+
+    return render_template('preview_post.html', post=session.get('temp_post'))
+
+
+@app.route("/delete_post/<int:post_id>")
+def delete_post(post_id):
+    post_to_delete = Post.query.get_or_404(post_id)
+    db.session.delete(post_to_delete)
+    db.session.commit()
+    return redirect(url_for('create_post'))
 
 
 @app.route('/contact')
