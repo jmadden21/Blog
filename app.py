d93a095a8cc583b142499ee28dbe54a5f3f85a3f
jmadde882 <jack_madden@comcast.com>
2023-12-01 11:40:11 -0500

    Add ability for user to post.

--- app.py
+++ app.py
@@ -1,29 +1,39 @@
+from datetime import datetime
+
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+db = SQLAlchemy(app)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
 
 
 @app.route('/')
 def home():
-    posts = [
-        {"title": "First Post", "content": "This is the content of my first post"},
-        {"title": "Second Post", "content": "This is the content of my second post"}
-    ]
+    posts = Post.query.all()
     return render_template('home.html', posts=posts)
 
 
 @app.route('/create_post', methods=['GET', 'POST'])
 def create_post():
     if request.method == 'POST':
-        # Handle the form submission
-        title = request.form['title']
-        content = request.form['content']
-        print("New Post Created:", title, content)
-        # Later, you'll save this data to a database
-
-        return redirect(url_for('home'))  # Redirect to the home page after submission
-
-    return render_template('create_post.html')  # Render the form page for GET requests
+        post_title = request.form['title']
+        post_content = request.form['content']
+        new_post = Post(title=post_title, content=post_content)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('create_post.html')
 
 
 @app.route('/contact')
